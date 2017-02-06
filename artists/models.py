7016a073fb3e7ddd16add152d91397177adab849
Ramsés Martinez <ramses.mtz96@gmail.com>
2017-02-05 20:43:47 -0600

    Added new apps

---
+++ artists/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Artist(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255, blank=True)
+    biography = models.TextField(blank=True)
+
+    class Meta:
+        verbose_name = "Artist"
+        verbose_name_plural = "Artists"
+
+    def __str__(self):
+        pass
+    
--- sfotipy/settings.py
+++ sfotipy/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +38,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+THIRD_APPS = []
+
+USER_APPS = [
+    'artists.apps.ArtistsConfig',
+    'tracks.apps.TracksConfig',
+    'albums.apps.AlbumsConfig'
+]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_APPS + USER_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
