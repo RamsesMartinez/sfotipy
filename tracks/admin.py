289f9b4d45c0ee534bc2d066e56d27e956fb9c6d
Ramsés Martinez <ramses.mtz96@gmail.com>
2017-02-07 00:04:52 -0600

    Added actions: export_asexcel

--- tracks/admin.py
+++ tracks/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
+from actions import export_as_excel
+
 from .models import Track
 
 
@@ -8,6 +10,8 @@ class TrackAdmin(admin.ModelAdmin):
     list_filter = ('artist', 'album')
     search_fields = ('title', 'artist__first_name', 'artist__last_name')
     list_editable = ('title', 'album')
+
+    actions = (export_as_excel, )
 
     def es_codlplay(self, obj):
         return obj.id == 1
